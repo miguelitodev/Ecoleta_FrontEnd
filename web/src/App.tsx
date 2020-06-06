c8f8487a8abcf81c08e164dafb69c3bb3ffb4460
miguelrisquelme <miguelnascimento22@hotmail.com>
2020-06-06 16:28:00 -0300

    Entendendo conceitos ReactJS

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Header from './Header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+	const [counter, setCounter] = useState(0)
+
+	function HandleButtonClick() {
+		setCounter(counter + 1);
+	}
+
+	return (
+		<div>
+			<Header title={`Contador: ${counter}`}/>
+			<button onClick={HandleButtonClick}>Clique em mim</button>
+		</div>
+	);
 }
 
 export default App;
